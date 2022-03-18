383079fc8ac4670354d380b58a5757b3132ec784
Bidoia <83676738+B1doia@users.noreply.github.com>
2022-03-18 14:02:59 -0300

    Create Laço_for.c

---
+++ Laço_for.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(void)
+  
+{
+  int i;//variável de controle do loop
+  
+  for (i = 1; i <= 3; i++)
+  {
+    printf("Aprendendo Linguagem C\n");
+  }
+  
+  getch();
+  return(0);
+}
